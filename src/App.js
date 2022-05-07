ab21614ed31003ceba85ce03fba97aa9a0a9c211
Morteza <mtoshe87@gmail.com>
2022-05-07 18:37:26 +0430

    Navbar completed!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return(
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>OO</span>DHUBS</a>
+                <div className="nav-links">
+                    <a href="#!" className="active">Home</a>
+                    <a href="#!">Recipes</a>
+                    <a href="#!">Settings</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
