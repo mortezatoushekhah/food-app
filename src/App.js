a0204e46e79cb0d5f28e36b82f0d3a28432e130e
Morteza <mtoshe87@gmail.com>
2022-05-11 00:11:05 +0430

    Final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import Navbar from "./components/navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
-  );
+      <div className="container main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
